36ebf8cf1287eaaedb54c0fb9e1d0b5256e4676b
simonemayers <simone21295@gmail.com>
2021-05-12 22:56:11 -0400

    commit tree e89baccbee8ed0ec599bbb856816b08f6e5955e0

--- index.js
+++ index.js
@@ -1,5 +1,56 @@
 /* Your Code Here */
 
+function createEmployeeRecord(record){
+    const employee = {
+        firstName: record[0],
+        familyName : record[1], 
+        title: record[2], 
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+}
+
+function createEmployeeRecords(records){
+    return records.map(record => {
+        return createEmployeeRecord(record)
+    })
+}
+
+function createTimeInEvent(time){
+    const TimeIn = {
+        type: "TimeIn", 
+        hour: parseInt(time.split(" ")[1]),
+        date: time.split(" ")[0]
+    }
+    this.timeInEvents.push(TimeIn)
+    return this
+}
+
+function createTimeOutEvent(time){
+    const TimeOut = {
+        type: "TimeOut", 
+        hour: parseInt(time.split(" ")[1]),
+        date: time.split(" ")[0]
+    }
+    this.timeOutEvents.push(TimeOut)
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    const timeIn = this.timeInEvents.find(e=> e.date === date)
+    const timeOut = this.timeOutEvents.find(e=> e.date === date)
+    return (timeOut.hour - timeIn.hour) * 0.01
+}
+
+
+function wagesEarnedOnDate(date){
+    const hours = hoursWorkedOnDate.call(this, date)
+    const rate = this.payPerHour
+    return rate * hours
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +70,13 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(arr, firstName){
+    return arr.find(employee => employee.firstName === firstName)
+}
+
+function calculatePayroll(records){
+    const pay = records.map(record => allWagesFor.call(record))
+    return pay.reduce((a, b) => a+b)
 }
